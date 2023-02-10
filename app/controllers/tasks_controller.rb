7ef42d5853a0791510d38e00aed6b9a5ba557a05
corleonyis <corleonis.nyi531@gmail.com>
2023-02-11 01:32:23 +0900

    09. タスクの詳細を表示する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,10 @@ class TasksController < ApplicationController
     redirect_to tasks_path, status: :see_other
 	end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+  
   private
     def task_params
       params.require(:task).permit(:title)
