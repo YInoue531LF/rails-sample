7ddc0af96bc49f7acb073da6b59b79a6cdba2689
corleonyis <corleonis.nyi531@gmail.com>
2023-02-28 22:15:16 +0900

    14. 既存モデルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_134243) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_130514) do
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.boolean "done", default: false, null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
