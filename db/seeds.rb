9b1a1dd85b4988eb079af6b07a5c52aa35ae891d
corleonyis <corleonis.nyi531@gmail.com>
2023-09-01 23:38:18 +0900

    18. seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 1件ずつ記述
+user = User.create(email: "user1@test.co.jp", password: "passUser1")
+Task.create(title: "0.タスク0", user_id: user.id)
+
+# まとめて記述
+10.times do |i|
+    Task.create(title: "#{i+1}. タスク#{i+1}", user_id: user.id)
+end
